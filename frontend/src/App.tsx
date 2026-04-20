f7c223e569777b0883225a1ecd74ff7307aa7a50
Prittstick22 <dev.bhatt67890@gmail.com>
2026-04-20 21:30:02 +0100

    Fix React dependany for build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Layout } from "./components/layout/Layout";
 import { GrowthCalculator } from "./components/calculators/GrowthCalculator";
 import { WithdrawalCalculator } from "./components/calculators/WithdrawalCalculator";
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { CurrencyProvider } from './context/CurrencyContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <CurrencyProvider>
       <App />
     </CurrencyProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
